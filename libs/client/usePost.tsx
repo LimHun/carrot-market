1d890fc6d6b49e9aeb9af05f8b5c5d25751e4e9f
tunko <tunko@Hun-Wifi.local>
2022-04-01 18:55:24 +0900

    typescript 제너릭 추가중

--- libs/client/usePost.tsx
+++ libs/client/usePost.tsx
@@ -1,4 +1,17 @@
+import { useState } from 'react';
 
-export default function usePost(url: string) {
-    return   
+interface UsePostState<T> {
+    loading: boolean;
+    data?: T;
+    error?: object;
+}
+
+type UsePostResult<T> = [(data: any) => void, UsePostState<T>];
+
+export default function usePost<T = any>(url: string): UsePostResult<T> {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState<undefined | any>(undefined);
+    const [error, setError] = useState<undefined | any>(undefined);
+    function post(data: any) {}
+    return [post, { loading, data, error }];
 }
