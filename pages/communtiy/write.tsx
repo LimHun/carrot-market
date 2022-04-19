47c752436c385bb475da4d9340326594e9cd8225
tunko <tunko@Hun-Wifi.local>
2022-04-19 17:28:23 +0900

    post write react hook form

--- pages/communtiy/write.tsx
+++ pages/communtiy/write.tsx
@@ -1,10 +1,48 @@
 import type { NextPage } from "next";
 import Layout from "@components/layout";
+import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+import { Post } from "@prisma/client";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+interface WriteForm {
+    question: string;
+}
+
+// 데이터를 받기 위해 만은 인터페이스
+interface WriteResponse {
+    ok: boolean;
+    post: Post;
+}
 
 const Write: NextPage = () => {
+    const [post, { loading, data }] = useMutation<WriteResponse>("/api/posts"); // 요건 걍 데이터를 불러오는거다.
+
+    const { register, handleSubmit } = useForm<WriteForm>();
+
+    const onValid = (data: WriteForm) => {
+        if (loading) return;
+        post(data);
+        console.log(`data.question : ${data.question}`);
+    };
+
+    const router = useRouter();
+    useEffect(() => {
+        if (data && data.ok) {
+            router.push(`/communtiy/${data.post.id}`);
+        }
+    }, [data, router]);
+
     return (
         <Layout title="동네 질문하기" canGoBack>
-            <form className="px-4 py-10">
+            <form onSubmit={handleSubmit(onValid)} className="px-4 py-10">
+                <TextArea
+                    register={register("question", { required: true, minLength: 5 })}
+                    required
+                    placeholder="Ask a question!"
+                />
                 <textarea
                     className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 "
                     rows={4}
@@ -15,7 +53,7 @@ const Write: NextPage = () => {
                 </button>
             </form>
         </Layout>
-    )
-}
+    );
+};
 
-export default Write
+export default Write;
